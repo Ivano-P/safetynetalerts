7c250821ad00cccb3745df2c6dffd204bff99067
Ivano PETTY <96083465+Ivano-P@users.noreply.github.com>
2023-04-02 08:57:10 +0200

    created model objects to pass json file data into

--- src/main/java/com/safetynet/safetynetalerts/SafetynetalertsApplication.java
+++ src/main/java/com/safetynet/safetynetalerts/SafetynetalertsApplication.java
@@ -1,5 +1,6 @@
 package com.safetynet.safetynetalerts;
 
+import com.safetynet.safetynetalerts.dao.ExtractObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SafetynetalertsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SafetynetalertsApplication.class, args);
+		ExtractObject extractObject = new ExtractObject();
+
+		extractObject.extractDataFromJason();
+
 	}
 
 }
--- src/main/java/com/safetynet/safetynetalerts/dao/ExtractObject.java
+++ src/main/java/com/safetynet/safetynetalerts/dao/ExtractObject.java
@@ -1,6 +1,44 @@
 package com.safetynet.safetynetalerts.dao;
 
+import com.jsoniter.JsonIterator;
+import com.jsoniter.any.Any;
+import com.safetynet.safetynetalerts.model.Firestation;
+import com.safetynet.safetynetalerts.model.MedicalRecord;
+import com.safetynet.safetynetalerts.model.Person;
+import com.safetynet.safetynetalerts.model.SafetyNet;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 public class ExtractObject {
+
+    public void extractDataFromJason(){
+        File jsonInput = new File("src/main/resources/data/data.json") ;
+        try(InputStream inputStream = new FileInputStream(jsonInput)) {
+            JsonIterator iterator = JsonIterator.parse(inputStream, 1024);
+            Any dataAny = iterator.readAny();
+            SafetyNet safetyNet = dataAny.as(SafetyNet.class);
+
+            List<Person> persons = safetyNet.getPersons();
+            List<Firestation> firestations = safetyNet.getFirestations();
+            List<MedicalRecord> medicalRecords = safetyNet.getMedicalrecords();
+
+            //testing that object is populated correctly
+            System.out.println(persons);
+            System.out.println(firestations);
+            System.out.println(medicalRecords);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void printLists(){
+
+    }
+
 
 }
